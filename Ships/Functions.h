3f862764ba5068849053ca6076587b7e9e58588e
drwoj <dr.woj@wp.pl>
2022-01-19 22:39:06 +0100

    Create Functions.h

---
+++ Ships/Functions.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "PLayer.h"
+#include "Leaderboard.h"
+#include <conio.h>
+
+using namespace std;
+
+void menu(Player& p, Bot& b, Leaderboard& l);
+
+
+void set(Player& p, Bot& b);
+
+
+void setb(Bot& b);
+
+
+void setp(Player& p);
+
+
+void battle(Player& p, Bot& b);
+
+
+void shots(Player& p, Bot& b);
+

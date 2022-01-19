41a7321be902e5cbdc36aac9097a9d91b76bb014
drwoj <dr.woj@wp.pl>
2022-01-19 22:39:03 +0100

    Create Header.h

---
+++ Ships/Header.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ShipPart.h"
+#include "Bullet.h"
+#include "Ship.h"
+#include "Board.h"
+#include "User.h"
+#include "Player.h"
+#include "Bot.h"
+#include "Leaderboard.h"
+#include "Functions.h"
+
+
